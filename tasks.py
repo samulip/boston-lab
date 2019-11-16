5e408d5ad4720ae76b018ec7b4ba5082e6baef7b
Samuli P <samulip@users.noreply.github.com>
2019-11-16 23:59:50 +0100

    Use invoke for automation and black for formatting

---
+++ tasks.py
@@ -0,0 +1,18 @@
+from invoke import task
+
+@task()
+def precommit(c):
+    format(c)
+    test(c)
+
+
+@task()
+def format(c):
+    c.run("black src tests setup.py tasks.py")
+
+
+@task()
+def test(c):
+    c.run("pytest tests")
+    c.run("pytest --nbval-lax notebooks/*.ipynb")
+
